971b9c4bc09ff1561f21ef08ee5b474a26cc2919
Kshitiz Bharati <kshitizxox@hotmail.com>
2024-02-04 16:52:47 +1100

    Added apollo client. Added an config file

---
+++ src/graphql.ts
@@ -0,0 +1,10 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+
+} from "@apollo/client";
+
+export const apolloClient = new ApolloClient({
+    uri: "http://localhost:4000/api",
+    cache: new InMemoryCache(),
+});
